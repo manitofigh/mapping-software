4ad2a7e345cbd74c7c38ed6482bc54eb243752d6
manitofigh <manitofigh@protonmail.com>
2024-03-04 19:36:15 -0500

    Added map to both portals and added css tweaks to make map dark mode

---
+++ adminMap.js
@@ -0,0 +1,14 @@
+// Creating map options
+var mapOptions = {
+   center: [40.7169, -73.5990],
+   zoom: 15
+}
+
+// Creating a map object
+var map = new L.map('adminMap', mapOptions);
+
+// Creating a Layer object
+var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+// Adding layer to the map
+map.addLayer(layer);
